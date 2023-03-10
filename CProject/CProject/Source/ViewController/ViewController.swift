9ea2c3527452ff9bd16d4f54911e6f970aaa6440
junha <goodjunha@gmail.com>
2023-03-10 09:47:39 +0900

    처음부터 다시 시작

---
+++ CProject/CProject/Source/ViewController/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  CProject
+//
+//  Created by 박준하 on 2023/03/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
